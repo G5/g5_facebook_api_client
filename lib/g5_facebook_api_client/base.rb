279760fdea85c1fecc73a83fdb5e189efcfa1d0f
bbauer <brian.bauer@g5platform.com>
2014-12-17 09:32:05 -0800

    Move base under module

--- lib/g5_facebook_api_client/base.rb
+++ lib/g5_facebook_api_client/base.rb
@@ -1,29 +1,31 @@
-class G5FacebookApiClient::Base
-  def initialize(client_id, client_secret, page=nil)
-    @client_id = client_id
-    @client_secret = client_secret
-    @page = page
-  end
+module G5FacebookApiClient
+  class Base
+    def initialize(client_id, client_secret, page=nil)
+      @client_id = client_id
+      @client_secret = client_secret
+      @page = page
+    end
 
-  def access_token
-    @access_token ||=
-      G5FacebookApiClient::AccessToken.new(@client_id, @client_secret).fetch
-  end
+    def access_token
+      @access_token ||=
+        G5FacebookApiClient::AccessToken.new(@client_id, @client_secret).fetch
+    end
 
-  def parse(body)
-    JSON.parse(body)
-  end
+    def parse(body)
+      JSON.parse(body)
+    end
 
-  def get(url)
-    HTTParty.get(encode(url)).body
-  end
+    def get(url)
+      HTTParty.get(encode(url)).body
+    end
 
-  def post(url)
-    HTTParty.post(encode(url)).body
-  end
+    def post(url)
+      HTTParty.post(encode(url)).body
+    end
 
-  def encode(string)
-    URI::encode(string)
+    def encode(string)
+      URI::encode(string)
+    end
   end
 end
 
